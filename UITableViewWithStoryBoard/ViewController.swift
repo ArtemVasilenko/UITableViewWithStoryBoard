6051964029858b6dbfed410d5f168f6f261179c0
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-17 20:27:56 +0300

    Initial Commit

---
+++ UITableViewWithStoryBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITableViewWithStoryBoard
+//
+//  Created by Артем on 5/17/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
