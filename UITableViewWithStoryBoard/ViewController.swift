69f18a1a9b975264b00817ae752e0959aa28c57c
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-21 13:57:02 +0300

    start

--- UITableViewWithStoryBoard/ViewController.swift
+++ UITableViewWithStoryBoard/ViewController.swift
@@ -1,20 +1,53 @@
-//
-//  ViewController.swift
-//  UITableViewWithStoryBoard
-//
-//  Created by Артем on 5/17/19.
-//  Copyright © 2019 Артем. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+    let id = "MyCell"
+    
+    var array = ["1", "2", "3", "4"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    
+    
+    @IBAction func editTable(_ sender: Any) {
+        myTableView.isEditing = !myTableView.isEditing
+    }
+    
+    
+}
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: id, for: indexPath)
+        cell.textLabel?.text = array[indexPath.row]
+        
+        return cell
+    }
+    
 
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            array.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .left)
+        }
+        
+    }
+    
 }
 
